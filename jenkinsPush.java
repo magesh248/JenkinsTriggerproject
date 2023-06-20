a17b2075998785ea459e8dd2225545111455a50e
Magesh <80446035+magesh248@users.noreply.github.com>
2023-06-20 11:35:28 +0530

    Update jenkinsPush.java

--- jenkinsPush.java
+++ jenkinsPush.java
@@ -1,1 +1,12 @@
-hello magesh
+public class jenkinsPush{
+
+
+public static void main(String[] args){
+
+System.out.println("First build done");
+System.out.println("Second build done");
+System.out.println("Third build done");
+
+}
+
+}
