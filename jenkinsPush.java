de0ab400f301ed60dea2426b87b1a5da5e4ceadf
Magesh <80446035+magesh248@users.noreply.github.com>
2023-06-20 08:38:31 +0530

    Add files via upload

---
+++ jenkinsPush.java
@@ -0,0 +1,12 @@
+public class jenkinsPush{
+
+
+public static void main(String[] args){
+
+system.out.println("First build done");
+system.out.println("Second build done");
+system.out.println("Third build done");
+
+}
+
+}
