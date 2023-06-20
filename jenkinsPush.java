e958c28a5a9ba123d4529ab34789bd34056a333b
Magesh <80446035+magesh248@users.noreply.github.com>
2023-06-20 10:08:05 +0530

    Update jenkinsPush.java

--- jenkinsPush.java
+++ jenkinsPush.java
@@ -4,6 +4,7 @@ public static void main(String[] args) {
 				
 		System.out.println("gitpush works on jenkins");
 	System.out.println("gitpush works on jenkins");
+	System.out.println("gitpush works on jenkins");
 	
 	System.out.println("Exited to work in continous integration tool via gitpush");
 
