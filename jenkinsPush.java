424ddf1afb4e55851a3c7cc53c02939df16dce3f
Magesh <80446035+magesh248@users.noreply.github.com>
2023-06-20 11:29:47 +0530

    Update jenkinsPush.java

--- jenkinsPush.java
+++ jenkinsPush.java
@@ -1,9 +1,18 @@
 public class jenkinsPush{
 
 public static void main(String[] args) {
+		
+			String originalstr ="PALINDROME";
+			String REVERSEstr =" ";
+			
+			for(int i=0;i<originalstr.length();i++)
+			{
 				
-System.out.prinln("Hi Magesh");
-System.out.prinln("Hi Magesh");
+				REVERSEstr=originalstr.charAt(i) + REVERSEstr;
+			}
+			
+		System.out.println(REVERSEstr);
 	}
 
 }
+
