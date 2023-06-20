e5c9c899b3cf033b38c96894d9b2160ea67a1eb2
Magesh <80446035+magesh248@users.noreply.github.com>
2023-06-20 11:38:46 +0530

    Update jenkinsPush.java

--- jenkinsPush.java
+++ jenkinsPush.java
@@ -5,7 +5,7 @@ public static void main(String[] args){
 
 System.out.println("First build done");
 System.out.println("Second build done");
-System.out.println("Third build done");
+//ERROR: Couldn't find any revision to build. Verify the repository and branch configuration for this job.
 
 }
 
